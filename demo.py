c0cedfe0ba25edb8d71598a23174435068bd742e
fredcommo <fredcommo@gmail.com>
2022-03-28 08:52:35 +0200

    Added demo.py to test installation

--- demo.py
+++ demo.py
@@ -1,6 +1,8 @@
 import numpy as np
 from kernel_smoother.smoother import kern_smooth
 
+np.random.seed(1)
+
 x = np.linspace(0, 10, num=200)
 noise = np.random.normal(loc=0, scale=0.25, size=len(x))
 y = np.sin(x) + noise
@@ -8,7 +10,9 @@ y = np.sin(x) + noise
 kernels = ["gaussian", "triangular", "Epanechnikov"]
 for k in kernels:
     y_smooth = kern_smooth(x, y, K=k)
-    print(np.mean((y_smooth - y)**2)*(1/2))
+    rmse = np.mean((y_smooth - y)**2)*(1/2)
+    print("Kernel: {}, RMSE: {:.3f}, Valid: {}" \
+        .format(k, rmse, rmse < 0.03))
 
 # plt.scatter(x, y, s=10, c='grey')
 
