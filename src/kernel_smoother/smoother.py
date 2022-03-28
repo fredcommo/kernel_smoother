70da5145f10c695af046513f4d61b3dde3aa016e
fredcommo <fredcommo@gmail.com>
2022-03-28 08:44:28 +0200

    Comment if __name__

--- src/kernel_smoother/smoother.py
+++ src/kernel_smoother/smoother.py
@@ -46,59 +46,59 @@ def kern_smooth(x, y, K="gaussian", h=None):
 
 
 
-if __name__ == "__main__": # pragma: no cover
-    import matplotlib.pyplot as plt
+# if __name__ == "__main__": # pragma: no cover
+#     import matplotlib.pyplot as plt
 
-    x = np.linspace(0, 10, num=200)
-    noise = np.random.normal(loc=0, scale=0.25, size=len(x))
-    y = np.sin(x) + noise
-
-
-    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(27, 8))
-
-    ############################################
-    # Compare banwidth effect on gaussian kernel
-    bwidths = [0.1, bandwidth(y), 0.5, 1, 2]
-    ax1.scatter(x, y, s=10, c='grey')
-    for h in bwidths:
-        ysmooth = kern_smooth(x, y, h=h)
-        ax1.plot(x, ysmooth, linewidth=3)
-    ax1.set_title("Bandwidth effect", fontsize=22)
-    ax1.legend([f"h = {h}" for h in bwidths], title="Gaussian")
-    ax1.set(xlabel='x', ylabel='y')
+#     x = np.linspace(0, 10, num=200)
+#     noise = np.random.normal(loc=0, scale=0.25, size=len(x))
+#     y = np.sin(x) + noise
 
 
-    ############################################
-    # Compare kernels
-    kernels = {
-        "gaussian": bandwidth(y),
-        "triangular": 1,
-        "Epanechnikov": 1
-        }
+#     fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(27, 8))
 
-    ax2.scatter(x, y, s=10, c='grey')
-    for kernel in kernels:
-        ysmooth = kern_smooth(x, y, K=kernel, h=kernels[kernel])
-        ax2.plot(x, ysmooth, linewidth=3)
-    ax2.set_title("Kernel effect", fontsize=22)
-    ax2.legend(kernels, title="Kernels")
-    ax2.set(xlabel='x', ylabel='y')
+#     ############################################
+#     # Compare banwidth effect on gaussian kernel
+#     bwidths = [0.1, bandwidth(y), 0.5, 1, 2]
+#     ax1.scatter(x, y, s=10, c='grey')
+#     for h in bwidths:
+#         ysmooth = kern_smooth(x, y, h=h)
+#         ax1.plot(x, ysmooth, linewidth=3)
+#     ax1.set_title("Bandwidth effect", fontsize=22)
+#     ax1.legend([f"h = {h}" for h in bwidths], title="Gaussian")
+#     ax1.set(xlabel='x', ylabel='y')
+
+
+#     ############################################
+#     # Compare kernels
+#     kernels = {
+#         "gaussian": bandwidth(y),
+#         "triangular": 1,
+#         "Epanechnikov": 1
+#         }
+
+#     ax2.scatter(x, y, s=10, c='grey')
+#     for kernel in kernels:
+#         ysmooth = kern_smooth(x, y, K=kernel, h=kernels[kernel])
+#         ax2.plot(x, ysmooth, linewidth=3)
+#     ax2.set_title("Kernel effect", fontsize=22)
+#     ax2.legend(kernels, title="Kernels")
+#     ax2.set(xlabel='x', ylabel='y')
     
-    ############################################
-    # Compare automated bandwidth on different variances
+#     ############################################
+#     # Compare automated bandwidth on different variances
 
-    x = np.linspace(0, 10, num=200)
-    S = [0.1, 0.3, 0.5]
-    for i, s in enumerate(S):
-        noise = np.random.normal(loc=0, scale=s, size=len(x))
-        y = np.sin(x) + i*3 + noise
-        h = bandwidth(y)
-        ysmooth = kern_smooth(x, y, K="gaussian", h=h)
-        ax3.scatter(x, y, s=10, c='grey')
-        ax3.plot(x, ysmooth, linewidth=3)
-        ax3.annotate("Found bdw: {:.3f}".format(h), (0, i*3 - 1), fontsize=14)
-    ax3.set_title("y-variance effect", fontsize=22)
-    ax3.legend(S, title="Variances")
-    ax3.set(xlabel='x', ylabel='y')
+#     x = np.linspace(0, 10, num=200)
+#     S = [0.1, 0.3, 0.5]
+#     for i, s in enumerate(S):
+#         noise = np.random.normal(loc=0, scale=s, size=len(x))
+#         y = np.sin(x) + i*3 + noise
+#         h = bandwidth(y)
+#         ysmooth = kern_smooth(x, y, K="gaussian", h=h)
+#         ax3.scatter(x, y, s=10, c='grey')
+#         ax3.plot(x, ysmooth, linewidth=3)
+#         ax3.annotate("Found bdw: {:.3f}".format(h), (0, i*3 - 1), fontsize=14)
+#     ax3.set_title("y-variance effect", fontsize=22)
+#     ax3.legend(S, title="Variances")
+#     ax3.set(xlabel='x', ylabel='y')
 
-    plt.savefig('./demo_plot.png')
+#     plt.savefig('./demo_plot.png')
